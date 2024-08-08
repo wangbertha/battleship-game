3a23e328d846b5c03089e973f302b9f27af1b09a
wangbertha <wangbertha@gmail.com>
2024-08-08 16:39:55 -0500

    Render real player's board to DOM

--- src/index.js
+++ src/index.js
@@ -1,36 +1,41 @@
 import { BattleshipGame } from './battleshipGame.js';
+import './style.css';
 
 const battleshipGame = BattleshipGame();
 
 function renderGame(bsG) {
-    // const { realPlayer, computerPlayer } = bsG;
-    // const realPlayerBoard = realPlayer.gameBoard;
-    // const realPlayerShips = realPlayer.ships;
-    // const computerPlayerBoard = computerPlayer.gameBoard;
-    // const computerPlayerShips = computerPlayer.ships;
+    const { realPlayer, computerPlayer } = bsG;
+    const realPlayerBoard = realPlayer.gameBoard;
+    const realPlayerShips = realPlayer.ships;
+    const computerPlayerBoard = computerPlayer.gameBoard;
+    const computerPlayerShips = computerPlayer.ships;
 
-    // const body = document.querySelector('body');
+    const body = document.querySelector('body');
 
-    // const gameWrapper = document.createElement('div');
-    // gameWrapper.classList.add('game-wrapper');
-    // body.appendChild(gameWrapper);
+    const gameWrapper = document.createElement('div');
+    gameWrapper.classList.add('game-wrapper');
+    body.appendChild(gameWrapper);
 
-    // const realPlayerBoardWrapper = document.createElement('div');
-    // realPlayerBoardWrapper.classList.add('realplayerboard-wrapper');
-    // gameWrapper.appendChild(realPlayerBoardWrapper);
+    renderBoard(realPlayerBoard);
+}
 
-    // console.log(realPlayerBoard.board)
-    console.log('hello');
-    // for (let i=0; i<realPlayerBoard.board.length; i++) {
-    //     const boardRow = document.createElement('div');
-    //     boardRow.classList.add('board-row');
-    //     for (let j=0; j<realPlayerBoard.board.length; j++) {
-    //         const boardCell = document.createElement('div');
-    //         boardCell.classList.add('board-cell');
-    //         boardRow.appendChild(boardCell);
-    //     }
-    //     realPlayerBoardWrapper.appendChild(boardRow);
-    // }
+function renderBoard(board) {
+    const gameWrapper = document.querySelector('.game-wrapper');
+    
+    const boardWrapper = document.createElement('div');
+    boardWrapper.classList.add('board-wrapper');
+    gameWrapper.appendChild(boardWrapper);
+
+    for (let i=0; i<board.board.length; i++) {
+        const boardRow = document.createElement('tr');
+        boardRow.classList.add('board-row');
+        for (let j=0; j<board.board.length; j++) {
+            const boardCell = document.createElement('td');
+            boardCell.classList.add('board-cell');
+            boardRow.appendChild(boardCell);
+        }
+        boardWrapper.appendChild(boardRow);
+    }
 }
 
 renderGame(battleshipGame);
